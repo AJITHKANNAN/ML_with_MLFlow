c78accacc42ba373fd4600c04a666436dc4de657
Ajith <ajithkannan13698@gmail.com>
2024-03-15 19:54:05 +0530

    Model Trainer phase

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,7 +1,7 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
 from mlProject.entity.config_entity import (DataIngestionConfig, DataValidationConfig, 
-                                            DataTransformationConfig)
+                                            DataTransformationConfig,  ModelTrainerConfig)
 # from mlProject.entity import Data
 
 class ConfigurationManager:
@@ -58,4 +58,25 @@ class ConfigurationManager:
         )
 
         return data_transformation_config
+    
+
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_training
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_name = config.model_name,
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema.name
+            
+        )
+
+        return model_trainer_config
 
