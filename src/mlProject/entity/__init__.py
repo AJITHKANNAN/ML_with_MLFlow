353b1b011bb9978ad7354e140e291f65a8de6f54
Ajith <ajithkannan13698@gmail.com>
2024-03-15 18:07:42 +0530

    Data Validation Phase

--- src/mlProject/entity/__init__.py
+++ src/mlProject/entity/__init__.py
@@ -3,8 +3,17 @@ from pathlib import Path
 
 
 @dataclass(frozen=True)
-class DataIngestionConfig: # Creating entity with some return objects 
+class DataIngestionConfig:
     root_dir: Path
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir: Path
+    all_schema: dict
